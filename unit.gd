dc5bf25f788954e3d8dc556874c2593884455404
Kurt <gtd@m.greenfly.io>
2024-01-01 13:37:25 -0500

    Select or deselect a unit
    Click on it == select, click somewhere else == deselect

--- unit.gd
+++ unit.gd
@@ -1,13 +1,42 @@
 extends Area2D
 
+var tile_size = 80
+var inputs = {
+    "right": Vector2.RIGHT,
+    "left": Vector2.LEFT,
+    "up": Vector2.UP,
+    "down": Vector2.DOWN,
+    }
+var selected = false
+var mouse_is_over_me = false
+
+@export var unit_name = "MyUnit"
+
+func _ready():
+    position = position.snapped(Vector2.ONE * tile_size)
+    position += Vector2.ONE * tile_size/2
+
+func _unhandled_input(event):
+    if not selected: return
+    for direction in inputs.keys():
+        if event.is_action_pressed(direction):
+            move(direction)
+
+func move(direction):
+    position += inputs[direction] * tile_size
 
 func _on_mouse_entered():
-    print("mouse entered")
-    print(self)
+    mouse_is_over_me = true
 
+func _on_mouse_exited():
+    mouse_is_over_me = false
 
-
-func _on_input_event(viewport, event, shape_idx):
+func _input(event):
     if event.is_action_pressed("click"):
-        print("clicked")
-        var mouse_position = get_viewport().get_mouse_position()
+        if mouse_is_over_me:
+            print("selected " + unit_name)
+            selected = true
+        else:
+            print("deselected " + unit_name)
+            selected = false
+
