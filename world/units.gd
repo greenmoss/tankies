fbd81ca0599739f8de99bbc6a34cf88dc35ff470
Kurt <gtd@m.greenfly.io>
2024-01-17 15:04:58 -0500

    Move turn code from units to world
    Other object, e.g. cities also follow turns

--- world/units.gd
+++ world/units.gd
@@ -1,43 +1,18 @@
 extends Node
 
-var turn_queues = {}
-
-var taking_turn = false
-
 var unit_scene = preload("res://unit/unit.tscn")
 
-func set_team_queue(team_name):
+func make_team_queue(team_name):
     var team_queue = []
     for unit in get_children():
         if unit.my_team != team_name: continue
         unit.reset_moves()
         team_queue.append(unit)
-    turn_queues[team_name] = team_queue
+    return(team_queue)
+    #turn_queues[team_name] = team_queue
 
-func end_turn():
-    taking_turn = false
-
-func start_turn():
-    taking_turn = true
-
-    set_team_queue(Global.human_team)
-    if turn_queues[Global.human_team] == []:
-        var new_unit = unit_scene.instantiate()
-        new_unit.my_team = Global.human_team
-        new_unit.position = Vector2(200, 200)
-        add_child(new_unit)
-    set_team_queue(Global.ai_team)
-
-func run_turn_loop():
-    var humans_done = true
-    for unit in turn_queues[Global.human_team]:
-        # this happens when we remove a unit
-        if not is_instance_valid(unit): continue
-
-        if unit.has_more_moves():
-            humans_done = false
-            break
-
-    var ai_done = true
-    if(humans_done and ai_done):
-        end_turn()
+func create(team_name, coordinates):
+    var new_unit = unit_scene.instantiate()
+    new_unit.my_team = team_name
+    new_unit.position = coordinates
+    add_child(new_unit)
--- world/world.gd
+++ world/world.gd
@@ -2,21 +2,19 @@ extends Node2D
 
 @export var turn_number = 0
 
+var turn_queues = {}
+
+var taking_turn = false
+
 func _ready():
     # source: https://stackoverflow.com/a/68675270/1090611
     SignalBus.unit_collided.connect(_unit_collided)
 
 func _physics_process(_delta):
-    if($units.taking_turn):
-        $units.run_turn_loop()
+    if(taking_turn):
+        run_turn_loop()
     else:
-        start_next_turn()
-
-func start_next_turn():
-    var previous_turn = turn_number
-    turn_number += 1
-    $TurnOverlay.display(previous_turn, turn_number)
-    $units.start_turn()
+        start_turn()
 
 func reset_group(group_name):
     for nodes in get_tree().get_nodes_in_group(group_name):
@@ -41,3 +39,36 @@ func _unit_collided(unit, target):
         return
 
     unit.deny_move()
+
+func set_team_turn_queue(team_name):
+    turn_queues[team_name] = $units.make_team_queue(team_name)
+
+## turns code
+func start_turn():
+    var previous_turn = turn_number
+    turn_number += 1
+    $TurnOverlay.display(previous_turn, turn_number)
+    taking_turn = true
+
+    set_team_turn_queue(Global.human_team)
+    if turn_queues[Global.human_team] == []:
+        $units.create(Global.human_team, Vector2(200, 200))
+        set_team_turn_queue(Global.human_team)
+    set_team_turn_queue(Global.ai_team)
+
+func run_turn_loop():
+    var humans_done = true
+    for unit in turn_queues[Global.human_team]:
+        # this happens when we remove a unit
+        if not is_instance_valid(unit): continue
+
+        if unit.has_more_moves():
+            humans_done = false
+            break
+
+    var ai_done = true
+    if(humans_done and ai_done):
+        end_turn()
+
+func end_turn():
+    taking_turn = false
