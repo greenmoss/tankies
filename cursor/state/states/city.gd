52c6738880603c8b0bd2653b6c71706d43441fa2
Kurt <gtd@m.greenfly.io>
2024-06-02 17:42:15 -0400

    Show units inside city popup
    Also allow the units to be selected

--- cursor/state/states/city.gd
+++ cursor/state/states/city.gd
@@ -2,16 +2,29 @@ extends "../common/unit.gd"
 
 var marked:City
 
+@onready var background = $detail/background
 @onready var build_progress = $detail/build_progress
 @onready var build_turns_remaining = $detail/build_turns_margin/build_turns_remaining
 @onready var detail = $detail
 @onready var icon = $detail/icon
-@onready var lists = $detail/lists
+@onready var units1 = $detail/units1
+@onready var units2 = $detail/units2
+@onready var unit_slots = [
+    $detail/units1/unit1,
+    $detail/units1/unit2,
+    $detail/units1/unit3,
+    $detail/units2/unit4,
+    $detail/units2/unit5,
+    $detail/units2/unit6,
+    $detail/units2/unit7,
+    $detail/units2/unit8,
+    $detail/units2/unit9,
+    ]
 
 var build_progress_max:float
 var build_progress_min:float
 var build_progress_animation_time = 0.0
-var build_progress_animation_duration = 0.75
+var build_progress_animation_duration = 1.5
 var build_progress_animation_pause = 0.5
 
 var tween_speed = 7.5
@@ -19,6 +32,53 @@ var alpha_tween:Tween
 var position_tween:Tween
 
 var default_tooltip_text = "Turns until built: "
+@onready var default_size_y = $detail/background.size.y
+
+var units:Array[Unit]
+
+
+func _on_unit_1_pressed():
+    activate_unit(0)
+
+
+func _on_unit_2_pressed():
+    activate_unit(1)
+
+
+func _on_unit_3_pressed():
+    activate_unit(2)
+
+
+func _on_unit_4_pressed():
+    activate_unit(3)
+
+
+func _on_unit_5_pressed():
+    activate_unit(4)
+
+
+func _on_unit_6_pressed():
+    activate_unit(5)
+
+
+func _on_unit_7_pressed():
+    activate_unit(6)
+
+
+func _on_unit_8_pressed():
+    activate_unit(7)
+
+
+func _on_unit_9_pressed():
+    activate_unit(8)
+
+
+func activate_unit(unit_number:int):
+    var unit_count = units.size()
+    # ignore buttons without an attached unit
+    if unit_number >= unit_count:
+        return
+    owner.state.mark_unit(units[unit_number])
 
 
 func enter():
@@ -35,18 +95,38 @@ func enter():
     build_progress.tooltip_text = default_tooltip_text+str(marked.build_remaining)
     build_turns_remaining.tooltip_text = default_tooltip_text+str(marked.build_remaining)
 
+    units = []
+    for unit in owner.units_under_mouse.keys():
+        if not owner.units_under_mouse[unit]: continue
+        if unit == null: continue
+        if not is_instance_valid(unit): continue
+        if unit.is_queued_for_deletion(): continue
+        units.append(unit)
+
+    for slot_index in unit_slots.size():
+        if slot_index >= units.size():
+            break
+        unit_slots[slot_index].texture_normal = units[slot_index].icon.texture
+        unit_slots[slot_index].modulate = units[slot_index].modulate
+        unit_slots[slot_index].flip_h = units[slot_index].icon.flip_h
+
+    if units.size() > units1.get_children().size():
+        units2.visible = true
+        detail.size.y += default_size_y / 2
+        background.size.y += default_size_y / 2
+
     owner.position = Vector2(marked.position.x - Global.half_tile_size, marked.position.y - Global.half_tile_size)
 
-    var right_extent = owner.position.x + lists.size.x
-    var bottom_extent = owner.position.y + lists.size.y
+    var right_extent = owner.position.x + detail.size.x
+    var bottom_extent = owner.position.y + detail.size.y
     var updated_x = detail.position.x
     var updated_y = detail.position.y
 
     if right_extent > get_viewport().size.x:
-        updated_x = detail.position.x - lists.size.x + Global.tile_size
+        updated_x = detail.position.x - detail.size.x + Global.tile_size
 
     if bottom_extent > get_viewport().size.y:
-        updated_y = detail.position.y - lists.size.y + Global.tile_size
+        updated_y = detail.position.y - detail.size.y + Global.tile_size
 
     detail.modulate.a = 0.0
     detail.visible = true
@@ -70,7 +150,13 @@ func enter():
 func exit():
     detail.visible = false
     detail.position = Vector2.ZERO
+    detail.size.y = default_size_y
+    background.size.y = default_size_y
     build_progress_animation_time = 0.0
+    units2.visible = false
+
+    for slot in unit_slots:
+        slot.texture_normal = null
 
 
 func handle_input(event):
@@ -91,6 +177,11 @@ func handle_input(event):
 
     if event.is_action_pressed("escape"):
         emit_signal("next_state", "none")
+        return
+
+    if event.is_action_pressed('next'):
+        owner.state.find_unit.nearby = null
+        emit_signal("next_state", "find_unit")
         return
 
 
